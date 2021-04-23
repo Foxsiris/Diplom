e9cc793af11a3034d93c72a2d33564a7b95b3574
Daniil <daniilfgs@gmail.com>
2021-04-23 10:01:45 +0400

    Футер с картой , карусель , карточки с новостями

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { NewsComponent } from './news/news.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        CarouselComponent,
+        AppointmentComponent,
+        NewsComponent,
+        FooterComponent
+    ],
   imports: [
     BrowserModule
   ],
